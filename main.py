4c3523c3bee2cb50ebf6e4fa907f4eee346ca957
Hakim Horairy <horairyhakim@gmail.com>
2024-01-10 17:06:21 +0100

    adding fitting feature

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import data_preparation
+import regression
 
-
+# datapreparation settings
 path_to_csv = "../vente_maillots_de_bain.csv"
 x_column_name, y_column_name = "Years", "Sales"
 percentage_split = 0.8
 
+# fitting settings
+learning_rate = 10**(-5)
+number_of_epochs = 100_000
 
 data_preparation_object = data_preparation.DataPreparation(path_to_csv, x_column_name, y_column_name, percentage_split)
-# data_preparation_object.show_dataset()
-data_preparation_object.split_data()
+regression_object = regression.Regression(data_preparation_object, learning_rate, number_of_epochs)
--- regression.py
+++ regression.py
@@ -1,4 +1,7 @@
 import numpy
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 class Regression:
 	def __init__(self, data_preparation_object, learning_rate, number_of_epochs):
 		self.dataset_train_df = data_preparation_object.dataset_train_df
@@ -12,17 +15,31 @@ class Regression:
 
 		self.a = 0
 		self.b = 0
+		self.fit()
 
 
 	def predict(self, x_value):
 		return self.a * x_value + self.b
 
 	def fit(self):
+		x_train = self.dataset_train_df["temporal_index"]
+		y_train = self.dataset_train_df["Sales"]
+
+		x_test = self.dataset_test_df["temporal_index"]
+		y_test = self.dataset_test_df["Sales"]
+
 		train_length = len(self.dataset_train_df)
-		for epoch in range(self.number_of_epochs):
-			self.a -= 2*self.learning_rate/train_length * #
-			self.b -= 2*self.learning_rate/train_length * #
+		for epoch in tqdm(range(self.number_of_epochs)):
+			self.a -= 2*self.learning_rate/train_length * numpy.sum(x_train *(self.a*x_train + self.b - y_train))
+			self.b -= 2*self.learning_rate/train_length * numpy.sum(self.a * x_train + self.b - y_train)
 
-	# def mse(self):
-	# 	return 
+		y_train_predicted = self.predict(x_train)
+		y_test_predicted = self.predict(x_test)
 
+		plt.figure(figsize=(15, 6))
+		plt.plot(x_train, y_train, "bo")
+		plt.plot(x_train, y_train_predicted, "b")
+
+		plt.plot(x_test, y_test, "ro")
+		plt.plot(x_test, y_test_predicted, "r")
+		plt.show()
